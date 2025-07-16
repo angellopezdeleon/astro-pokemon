4bc5179ce9e8e94f071a62f6ffc09b8518eef21d
Angel Lopez <angel.lopjar@gmail.com>
2025-07-16 20:31:22 +0200

    second commit

---
+++ src/consts/site-info.ts
@@ -0,0 +1,4 @@
+export const siteInfo = {
+    title: "Pokemon Static",
+    description: "A static site to showcase Pokemon using Astro and PokeAPI.",
+};
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: { extend: {} },
+  plugins: [],
+};
+// This configuration file sets up Tailwind CSS for an Astro project.
+// It specifies the content files to scan for class names, extends the default theme, and includes
